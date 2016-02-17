3ef399551fe2430ec7a7b07744b4ad875c0aaab4
Echoffee <light.runa@gmail.com>
2016-02-17 12:35:33 +0100

    Fix: Vérification du malloc

--- game.c
+++ game.c
@@ -14,9 +14,11 @@ struct game_s{
 game new_game_hr( int nb_pieces, piece *pieces)
 {
 	game new_game = (game_s) malloc(sizeof(struct game_s));
-	isAllocated(new_game);
+	if(!new_game)
+		error("Allocation new_game");
 	new_game -> pieces = (piece*) malloc(sizeof(struct piece_s) * nb_pieces);
-	isAllocated(new_game -> pieces)
+	if(!new_game -> pieces)
+		error("Allocation new_game -> pieces");
 	new_game -> nb_pieces = nb_pieces;
 
 	for (int i = 0; i < nb_pieces; i++)
@@ -55,28 +57,25 @@ int game_nb_pieces(cgame g)
 }
 
 cpiece game_piece(cgame g, int piece_num){
-  //Vérifie que la pièce est bien dans le jeu
+	//Vérifie que la pièce est bien dans le jeu
 	if (piece_num < 0 || piece_num > game_nb_pieces(g)){
-		fprintf(stderr,"L'index de la pièce recherchée est impossible (trop grand ou négatif\n");
+		fprintf(stderr,"L'index de la pièce recherchée est impossible (trop grand ou négatif)\n");
 			exit(EXIT_FAILURE);
 		}
-  //Si elle existe, on la renvoie
-		return g -> pieces[piece_num];
-	}
+	//Si elle existe, on la renvoie
+	return g -> pieces[piece_num];
+}
 
 
-	bool game_over_hr(cgame g){
-		if (g -> pieces[0].position[0] = 4 && g -> pieces[0].position[1] = 3){
-			return true;
-		}
-		return false;
+bool game_over_hr(cgame g){
+	if (g -> pieces[0].position[0] = 4 && g -> pieces[0].position[1] = 3){
+		return true;
 	}
+	return false;
+}
 
-	void isAllocated(void* pointer)
-	{
-		if (pointer == NULL)
-		{
-			printf("Allocation Error - Abort\n");
-			exit(EXIT_FAILURE);
-		}
-	}
+void error(char* s)
+{
+	printf("Fatal Error : %s.\n", s);
+	exit(EXIT_FAILURE);
+}
