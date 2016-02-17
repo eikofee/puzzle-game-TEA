bdcca933abb28b8e24321b59583a8d40651ed281
Apodeus <ordonez.romain@gmail.com>
2016-02-17 11:17:21 +0100

    MaJ: move_piece, switchsMovePiece

--- piece.c
+++ piece.c
@@ -14,9 +14,9 @@ piece new_piece_rh (int x, int y, bool small, bool horizontal){
 	if(!estAlloue(newPiece))
 		exit(EXIT_FAILURE);
 
-	if(x < 0 || y < 0)
+	if(x < 0 || x > 5 || y < 0 || y > 5)
 	{
-		fprintf(stderr,"Erreur: x ou y inférieur à 0\n");
+		fprintf(stderr, "Erreur: x ou y inférieur à 0\n");
 		exit(EXIT_FAILURE);
 	}
 
@@ -51,15 +51,95 @@ void copy_piece (cpiece src, piece dst){
 void move_piece (piece p, dir d, int distance){
 	if(!estAlloue(p))
 		exit(EXIT_FAILURE);
+	//A ce stade, p est bien alloué.
 
-	if( (p->isHorizontal && ((d == UP) || (d == DOWN))) || (!p->isHorizontal && ((d == RIGHT) || (d == LEFT))))
+	if(!estPositionValide(p))
 	{
-		fprintf(stderr,"Erreur: direction invalide avec cette pièce\n");
+		fprintf(stderr, "Erreur: Position de la piece invalide\n")
 		exit(EXIT_FAILURE);
 	}
-	//a finir
+	//A ce stade, p est bien positionnée.
+	if( (p->isHorizontal && ((d == UP) || (d == DOWN))) || (!p->isHorizontal && ((d == RIGHT) || (d == LEFT))))
+	{
+		// fprintf(stderr,"Erreur: direction invalide avec cette pièce\n");
+		// exit(EXIT_FAILURE);
+		return;
+	}
+	//A ce stade, les paramètres sont cohérents avec l'orientation de p.
+
+
+	// Dans le cas ou la piece fait 2 cases :
+	if(p -> isSmall)
+	{
+		switch (d){
+
+			case UP:
+				if((p -> position[1] + distance + 1) <= 5)
+					p -> position[1] += distance;
+				break;
+
+			case DOWN:
+				if((p -> position[1] - distance) >= 0)
+					p -> position[1] -= distance;
+				break;
+
+			case RIGHT:
+				if((p -> position[0] + distance + 1) <= 5)
+					p -> position[0] += distance;
+				break;
+
+			case LEFT:
+				if((p -> position[0] - distance) >= 0)
+					p -> position[0] -= distance;
+				break;
+
+			default:
+				printf("Cas non prévu\n");
+				break;
+		}
+	}
+
+	// Dans le cas ou la piece fait 3 cases :
+	if(!(p -> isSmall))
+	{
+		switch (d){
+
+			case UP:
+				if((p -> position[1] + distance + 2) <= 5)
+					p -> position[1] += distance;
+				break;
+
+			case DOWN:
+				if((p -> position[1] - distance) >= 0)
+					p -> position[1] -= distance;
+				break;
+
+			case RIGHT:
+				if((p -> position[0] + distance + 2) <= 5)
+					p -> position[0] += distance;
+				break;
+
+			case LEFT:
+				if((p -> position[0] - distance) >= 0)
+					p -> position[0] -= distance;
+				break;
+
+			default:
+				printf("Cas non prévu\n");
+				break;
+		}
+	}
+
 }
 
+//Vérifie si la position de la piece est bien dans le plateau.
+bool estPositionValide(piece p){
+	if((p -> position[0] < 0) || (p -> position[0] > 5) || (p -> position[1] < 0) || (p -> position[1] > 5))
+		return false;
+	return true;
+}
+
+//Verifie si l'allocation de la piece p s'est bien déroulée.
 bool estAlloue(piece p){
 	if(p == NULL)
 	{
@@ -67,4 +147,39 @@ bool estAlloue(piece p){
 		return false;
 	}
 	return true;
+}
+
+void switchsMovePiece(piece p, dir d, int distance){
+	int taille_piece;
+	if(p->isSmall)
+		taille_piece = 1;
+	else
+		taille_piece = 2;
+
+	switch (d){
+
+			case UP:
+				if((p -> position[1] + distance + taille_piece) <= 5)
+					p -> position[1] += distance;
+				break;
+
+			case DOWN:
+				if((p -> position[1] - distance) >= 0)
+					p -> position[1] -= distance;
+				break;
+
+			case RIGHT:
+				if((p -> position[0] + distance + taille_piece) <= 5)
+					p -> position[0] += distance;
+				break;
+
+			case LEFT:
+				if((p -> position[0] - distance) >= 0)
+					p -> position[0] -= distance;
+				break;
+
+			default:
+				printf("Cas non prévu\n");
+				break;
+		}
 }
