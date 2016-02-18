d06598d2d92ab835f56a351d37299aeaffa07c07
Echoffee <light.runa@gmail.com>
2016-02-18 17:06:14 +0100

    Maj: Formatage

--- game-test.c
+++ game-test.c
@@ -6,7 +6,7 @@
 int main(int argc, char* argv[])
 {
 	printf("> Préparation des tests:\n");
-	printf("> création des pièces...");
+	printf("> création des pièces...\n");
 	int nb_pieces = 4;
 	piece *p = (piece*) malloc(sizeof( struct piece_s) * nb_pieces);
 	p[0] = new_piece_rh(0, 3, true, true);		//Voiture rouge
@@ -15,21 +15,21 @@ int main(int argc, char* argv[])
 	p[3] = new_piece_rh(0, 0, false, true);		//Camion horizontal en 0,0
 	printf("Done.\n");
 
-	printf("> new_game_hr...");
+	printf("> new_game_hr...\n");
 	game g = new_game_hr(nb_pieces, p);
 	printf("Done.\n");
 
-	printf("> copy_game...");
+	printf("> copy_game...\n");
 	game g2;
 	copy_game(g, g2);
 	printf("Done.\n");
 
-	printf("> delete_game...");
+	printf("> delete_game...\n");
 	delete_game(g2);
 	printf("Done.\n");
 
-	printf("> game_nb_piece...");
-	int a = game_nb_piece(g);
+	printf("> game_nb_piece...\n");
+	int a = game_nb_pieces(g);
 	printf("Done, attendu = %d, reçu = %d.\n", nb_pieces, a);
 
 
