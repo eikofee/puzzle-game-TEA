f3a2f26979693dc0e40c84c214ad27b3d38f7b9c
Echoffee <light.runa@gmail.com>
2016-02-19 12:25:12 +0100

    MaJ: Lecture de niveaux

--- interface_txt.c
+++ interface_txt.c
@@ -1,32 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "game.c"
+#include "utility.h"
 
 void draw_interface();
 
+void display_pieces(piece *p, int taille)
+{
+	for (int i = 0; i < taille; i++)
+	{
+		printf("Pièce [%d] : x = %d, y = %d\n",i ,get_x(p[i]), get_y(p[i]));
+	}
+
+}
+game test_level()
+{
+
+	int nb_pieces = 4;
+	piece p[nb_pieces];
+	p[0] = new_piece_rh(0, 3, true, true);		//Voiture rouge
+	p[1] = new_piece_rh(2, 2, false, false);	//Camion jaune sur l'ex
+	p[2] = new_piece_rh(5, 2, true, false);		//Voiture verte sur l'ex
+	p[3] = new_piece_rh(0, 0, false, true);		//Camion horizontal en 0,0
+	game g = new_game_hr(nb_pieces, p);
+
+	return g;
+}
+
 int main(int argc, char* argv[])
 {
-	
-	draw_interface();
+	game g = test_level();
+	//display_pieces(g -> pieces, g -> nb_pieces);
+	draw_interface(g);
 	return 0;
 }
 
-void draw_interface()
+void draw_interface(game g)
 {
 	//Test values
 	int moves = 3;
 	int num_moves = 10;
 	char* seed = "0b4aaf345e";
-
-
+	int** t = TableauDePieces(g -> pieces, g -> nb_pieces);
 	printf("############### Rush Hour\n");
+
 	for (int i = 5; i > -1; i--)
 	{
 		printf("# ");
 		for (int j = 0; j < 6; j++)
 		{
-			//Check if there's a car here, and if not :
-			printf(".");
-			//end if
+			if (t[j][i] == -1)
+				printf(".");
+			else
+			{
+				printf("%d", t[j][i]);
+			}
 			printf(" ");
 		}
 		
@@ -41,11 +69,14 @@ void draw_interface()
 			case 3: //Exit
 				printf(">\n");
 				break;
+			case 0: 
+				printf("# Type 'help' for more commands\n");
+				break;
 			default:
 				printf("#\n");
 				break;
 		}
 	}
 	printf("###############\n");
-	printf("Enter the car's number you want to move or 'help' :\n");
+	printf("Enter the car's number you want to move :\n");
 }
