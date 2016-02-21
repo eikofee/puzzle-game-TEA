8d79d22d6f4cb0639ab68b6cad3468d0096cc6ca
Echoffee <light.runa@gmail.com>
2016-02-21 16:54:49 +0100

    MaJ: Interface presque fonctionnelle

--- src/interface_txt.c
+++ src/interface_txt.c
@@ -4,6 +4,7 @@
 #include "utility.h"
 
 void draw_interface();
+void input_player();
 
 void display_pieces(piece *p, int taille)
 {
@@ -31,7 +32,11 @@ int main(int argc, char* argv[])
 {
 	game g = test_level();
 	//display_pieces(g -> pieces, g -> nb_pieces);
-	draw_interface(g);
+	while (!game_over_hr(g))
+	{
+		draw_interface(g);
+		input_player(g);
+	}
 	return 0;
 }
 
@@ -61,22 +66,110 @@ void draw_interface(game g)
 		switch(i){
 			//let's check if we have to display more informations on the right:
 			case 4:	//Move num display
-				printf("# Move %d / %d\n", moves, num_moves);
-				break;
+			printf("# Move %d / %d\n", moves, num_moves);
+			break;
 			case 1:	//Level's seed display (optional)
-				printf("# Seed: %s\n", seed);
-				break;
+			printf("# Seed: %s\n", seed);
+			break;
 			case 3: //Exit
-				printf(">\n");
-				break;
+			printf(">\n");
+			break;
 			case 0: 
-				printf("# Type 'help' for more informations\n");
-				break;
+			printf("# Type 'help' for more informations\n");
+			break;
 			default:
-				printf("#\n");
-				break;
+			printf("#\n");
+			break;
 		}
 	}
 	printf("###############\n");
 	printf("Enter the car's number you want to move :\n");
 }
+
+bool str_equal(char* a, char* b)
+{
+	int i = 0;
+	while (a[i] != '\n')
+	{
+		if (a[i] != b[i])
+			return false;
+		i++;
+	}
+	return true;
+}
+
+bool isNumber(char s, int max_number)
+{
+	return (s >= 48 && s <= 48 + max_number);
+}
+bool isOperatorSimple(char s)
+{
+	return (s == '+' || s == '-');
+}
+int getNumber(char s)
+{
+	return s - 48;
+}
+dir getDirection(piece p, char sign)
+{
+	if (sign == '+')
+	{
+		if (p -> isHorizontal)
+			return RIGHT;
+		else{
+			return UP;
+		}
+	}else{
+		if (p -> isHorizontal)
+			return LEFT;
+		else{
+			return DOWN;
+		}
+	}
+}
+
+void input_player(game g)
+{
+	char input[6];
+	fgets(input, 5, stdin);
+
+	if (str_equal(input, "help"))
+	{
+		//Display help
+	}
+	if (str_equal(input, "hint"))
+	{
+		//Do a move 
+	}
+	if (str_equal(input, "exit"))
+	{
+		exit;
+	}
+	if (str_equal(input, "save"))
+	{
+		//sauvegarde
+	}
+	if (isNumber(input[0], g -> nb_pieces))
+	{
+		//correct input
+		/*SYNTAXE :
+			0 2 : Avance la voiture rouge de 2 cases vers la droite
+			1 -1 : Recule la voiture 1 vers le bas si verticale ou la gauche si horizontale
+			2 : Demande un déplacement de la voiture 2 (nouveau scanf)
+		*/ 
+			//syntaxe check
+			if (input[1] == '\n')
+			{
+				//ask for second input
+			}else{
+				if (input[1] == ' ' && isNumber(input[2], 9))
+				{
+					//TODO : Check if piece actually exists
+					move_piece(g -> pieces[getNumber(input[0])], getDirection(g -> pieces[getNumber(input[0])], '+'), getNumber(input[2]));
+				}
+			}
+		}else{
+			printf("Incorrect input.\n");
+		}
+
+	}
