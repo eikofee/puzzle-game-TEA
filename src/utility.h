e203088cdb0afca6c6bf19846adbe7b051f4d2c1
Apodeus <ordonez.romain@gmail.com>
2016-02-21 17:10:19 +0100

    MaJ: Ajouts de prototypes. Utility.h est maintenant le fichier .h principal, il appelle les autres .h

--- src/utility.h
+++ src/utility.h
@@ -1,5 +1,14 @@
 #ifndef _UTILITY_H_
 #define _UTILITY_H_
+
+#include "game.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void switchsMovePiece(piece p, dir d, int distance);
+bool estPositionValide(piece p);
 int** TableauDePieces(piece* tab_pieces, int taille);
 int** pieceEnTableau(piece p, int taille);
+void error(char* s);
+
 #endif
