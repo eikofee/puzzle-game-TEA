17062bb4f19c2a291a349ae286f2d7d17c41608f
tlanvaldear <martyoan@gmail.com>
2016-03-16 06:39:27 +0900

    Maj: Headers manquants & CMakeLists.txt
    retrait de piece_ar.c qui n'a l'air de servir à rien (?)

---
+++ lib/game_ar.h
@@ -0,0 +1,7 @@
+#ifndef _GAME_AR_H_
+#define _GAME_AR_H_
+
+game new_game_ar( int nb_pieces, piece *pieces);
+bool game_over_ar(cgame g);
+
+#endif
---
+++ lib/game_rh.h
@@ -0,0 +1,7 @@
+#ifndef _GAME_RH_H_
+#define _GAME_RH_H_
+
+game new_game_hr( int nb_pieces, piece *pieces);
+bool game_over_hr(cgame g);
+
+#endif
