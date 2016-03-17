c722f9f0c25127ed4bfbe2be6496bcbb63b4d981
Apodeus <ordonez.romain@gmail.com>
2016-03-17 10:42:00 +0100

    Rush Hour refonctionne !
    LoadTheGame refonctionne pour rushHour
    WiP : Convertir les anciennes ID

--- src/main/rush-hour.c
+++ src/main/rush-hour.c
@@ -9,7 +9,7 @@ void loadTheGame(char* id_src)
 {
 	//On crée le game g à partir d'un id source
 	printf("\nLoading Game ...\n");
-	game g = NULL;//getGameFromId(id_src);
+	game g = getGameFromId(id_src);
 	printf("Done\n\n");
 
 	//On malloc un id qu'on utilisera pour etre modifié pendant la partie
@@ -17,7 +17,7 @@ void loadTheGame(char* id_src)
 	if(id == NULL)
 		error("getIdFromGame(), probleme d'allocation memoire");
 
-	//getIdFromGame(g, id);
+	getIdFromGame(g, id);
 	draw_interface(g, id);
 
 	while (!game_over_hr(g))
@@ -27,7 +27,7 @@ void loadTheGame(char* id_src)
 		draw_interface(g, id);
 	}
 
-	//getIdFromGame(g, id);
+	getIdFromGame(g, id);
 
 	printf("************\n*--- GG ---*\n************\n");
 
@@ -37,14 +37,14 @@ void loadTheGame(char* id_src)
 
 int main(int argc, char* argv[])
 {
-	
-	// char* Game1 = "4103300222052";
+
+	// char* Game1 = "410300222052";
+	char* Game1 = "4n6x6p2w2h1x0y3p2w3h1x0y0p1w1h3x2y2p1w1h2x5y2";
 	// char* Game2 = "8103022024034144301040250";
 	// char* Game3 = "8123024012040042145250301";
 	
-	//loadTheGame(Game1);
+	loadTheGame(Game1);
 	//loadTheGame(Game2);
 	//loadTheGame(Game3);
-	printf("WIP\n");
 	return 0;
 }
